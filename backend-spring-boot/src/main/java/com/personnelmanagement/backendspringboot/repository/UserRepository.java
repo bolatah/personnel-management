3f61f80d2d50a31b38ee46469ed2420168e4a6c3
bolat <bolatah@outlook.com>
2024-03-16 17:21:46 +0100

    Revert "revised"
    This reverts commit 0ac828e843f6309e75d2037550f2fd92a944727d.

---
+++ backend-spring-boot/src/main/java/com/personnelmanagement/backendspringboot/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.personnelmanagement.backendspringboot.repository;
+
+import com.personnelmanagement.backendspringboot.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+ @Repository 
+public interface UserRepository extends MongoRepository<User, String> {
+    // Custom query method to find users by a specific role
+    List<User> findByRole(String role);
+
+    // Custom query method to find users by employeeId
+    Optional<User> findByEmployeeId(String employeeId);
+}
