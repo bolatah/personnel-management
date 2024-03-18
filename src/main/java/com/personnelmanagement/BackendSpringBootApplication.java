00cc97441fa8d539d35b72ad16ffd68e87906631
bolat <bolatah@outlook.com>
2024-03-18 14:50:38 +0100

    revised

--- src/main/java/com/personnelmanagement/BackendSpringBootApplication.java
+++ src/main/java/com/personnelmanagement/BackendSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.personnelmanagement.backendspringboot;
+package com.personnelmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
