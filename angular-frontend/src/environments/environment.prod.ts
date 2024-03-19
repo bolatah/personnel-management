f8623ebaa2789b84f3a2ec3cc03ee0ad7667749a
bolat <bolatah@outlook.com>
2024-03-19 21:48:12 +0100

    revised

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
- /*  apiEndpoint: "https://personnel-management-08584936e9f3.herokuapp.com/api/", */
-  apiEndpoint : "http://localhost:8080/api/"
+  apiEndpoint: "https://personnel-management-08584936e9f3.herokuapp.com/api/", 
+/*   apiEndpoint : "http://localhost:8080/api/" */
 };
