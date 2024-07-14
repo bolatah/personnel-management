c9c305388d6596cdbad777581d6c2bffa83f7902
Bolat <bolatah@outlook.com>
2024-07-14 22:42:21 +0200

    do some changes

---
+++ src/main/java/com/personnelmanagement/BackendSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.personnelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBootApplication.class, args);
+	}
+
+}
